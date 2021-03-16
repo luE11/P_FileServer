7bb289db91da48da20a2c77048c60a55fc73d7fa
Luis Martinez <luisedu555@gmail.com>
2021-03-16 12:03:53 -0500

    Agregadas rutas estáticas

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,6 @@ COPY package*.json ./
 RUN apk update && npm install
 RUN npm install pm2 -g
 
-RUN mkdir /usr/src/server_data
-
 COPY . .
 
 ENTRYPOINT [ "pm2-runtime", "index.js" ]
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require('fs')
 
 app.use(bodyParser.json( {limit: "50mb"}))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Ok')
@@ -16,7 +17,7 @@ app.post('/savephoto', (req, res) => {
   let filePath = req.body.path
   let base64buffer = req.body.image
   let bufferObj = Buffer.from(base64buffer, 'base64')
-  fs.writeFile(filePath, bufferObj, function (err) {
+  fs.writeFile(__dirname+filePath, bufferObj, function (err) {
     if(err){
       console.log("Error al guardar la imagen");
     }else{
